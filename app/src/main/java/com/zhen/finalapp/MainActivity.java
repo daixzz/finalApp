d43b7b40e277901c4f52788f60cf8fda32aac3a6
dxzz <1095212454@qq.com>
2020-06-05 23:04:04 +0800

    6.5没连上网

--- app/src/main/java/com/zhen/finalapp/MainActivity.java
+++ app/src/main/java/com/zhen/finalapp/MainActivity.java
@@ -1,14 +1,124 @@
 package com.zhen.finalapp;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.os.PersistableBundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity implements Runnable {
+
+
+    private final String TAG = "Rate";
+    String updateDate = "";
+    EditText movie_keyw;
+
+    Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        movie_keyw = (EditText) findViewById(R.id.keyWord);
+
+
+        Thread t = new Thread(this);
+        t.start();
+
+        handler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+
+
+                super.handleMessage(msg);
+            }
+        };
+    }
+
+
+    public void openOne(View btn) {
+
+    }
+
+    public void onClick(View btn) {
+
+    }
+
+    private void openConfig() {
+
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void run() {
+
+        Log.i(TAG, "run: run()......");
+        for (int i = 1; i < 3; i++) {
+            Log.i(TAG, "run: i=" + i);
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            Message msg = handler.obtainMessage(5);
+//msg.what = 5;
+            msg.obj = "Hello from run()";
+            handler.sendMessage(msg);
+
+
+            Document doc = null;
+            try {
+                doc = Jsoup.connect("https://movie.douban.com/top250").get();
+                Log.i(TAG, "run: " + doc.title());
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+        }
+
+
     }
 }
